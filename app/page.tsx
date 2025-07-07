0521882b81fb722578b9ad87142aee0dcb6f52ad
saifkazi25 <m.saifkazi@gmail.com>
2025-07-07 16:15:17 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import React, { Suspense } from 'react';
-import SelfieContent from '../selfie-content';
+import SelfieContent from './selfie-content';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-white text-black">
-      <Suspense fallback={<p>Loading form...</p>}>
-        <SelfieContent />
-      </Suspense>
+      <SelfieContent />
     </main>
   );
 }
+
 
 
 
